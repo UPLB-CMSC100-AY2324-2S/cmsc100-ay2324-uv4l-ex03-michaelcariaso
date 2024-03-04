83abd7db1cfd1dea9036e5bb2b631403b043190b
michaelcariaso <mdcariaso@up.edu.ph>
2024-03-04 15:04:02 +0800

    chore: Created more boolean variables and added a condition and nested loop in the first function for checking if the password contains the required characters

--- README.md
+++ README.md
@@ -13,3 +13,4 @@
 2. Edited README.md and created js file
 3. Created the three initial functions to be implemented
 4. Initialized some boolean variables in the validatePassword function for checking the two string parameters
+5. Created more boolean variables and added a condition and nested loop in the first function for checking if the password contains the required characters 
--- index.js
+++ index.js
@@ -19,6 +19,39 @@ function validatePassword(password1, password2){
         match = true; //same string
     }
 
+    //check length
+    length = password1.length
+
+    //for checking if there are at least 8 characters in the string
+    if(length >= 8){
+        minimum = true; //at least 8 characters
+    }
+
+    //list of numbers for checking
+    const digits = [0,1,2,3,4,5,6,7,8,9];
+
+    //count of numbers (10)
+    const num_count = 10;
+
+
+    //other boolean
+    contains_num = false;
+    has_uppercase = false;
+    has_lowercase = false;
+
+    //outer loop for checking all of the characters in the string
+    for(let i = 0; i < length; i++){
+        //inner loop for checking using the list of digits
+        for(let j = 0; j < num_count; j++){
+            //checking if one of the chars is equal to any of the string
+            if(password1[i] == digits[j]){
+                contains_num = true
+                break;
+            }
+        }
+    }
+
+
     
 }
 
